2e436a89197935a58f4d2066b9d7bb16d9b599f7
anonymmized <lih0deev@inbox.ru>
2025-09-30 22:04:13 +0300

    add total lines

--- counter.sh
+++ counter.sh
@@ -3,6 +3,7 @@
 scan_dir() {
     local DIR EXT 
     local cnt=0
+    local total_cnt=0
 
     while true; do
         read -r -p "Enter the full path to the directory: " DIR
@@ -15,13 +16,15 @@ scan_dir() {
 
     read -r -p "Enter extension for which to perform a script (py | cpp | pdf): " EXT
 
-    find "$DIR" -type f -name "*.$EXT" | while IFS= read -r f; do
+    while IFS= read -r -d '' f; do
         ((cnt++))
         lines_cnt=$(wc -l < "$f")
-        echo "For found file $f total lines: $lines_cnt"
-    done
+        total_cnt=$((total_cnt + lines_cnt))
+        printf "For found file %s total lines: %d\n" "$f" "$lines_cnt"
+    done < <(find "$DIR" -type f -name "*.$EXT" -print0)
 
     echo "Total files found: $cnt"
+    echo "Total lines found: $total_cnt"
 }
 
 scan_dir
